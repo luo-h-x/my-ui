8df6ac11ff76dd285c1eae724ffd0915c79d99df
luo-h-x <1877430627@qq.com>
2020-09-07 23:50:51 +0800

    main

---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <main class="my-main">
+    <slot></slot>
+  </main>
+</template>
+<style scoped lang="scss">
+.my-main {
+  display: block;
+  flex: 1;
+  flex-basis: auto;
+  padding: 20px;
+}
+</style>
