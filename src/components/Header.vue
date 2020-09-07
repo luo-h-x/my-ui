05e564b5f583ae71a3f6e718f4085ba9061cd7d7
luo-h-x <1877430627@qq.com>
2020-09-07 23:50:37 +0800

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside class="my-header" v-if="visible">
+    <slot></slot>
+  </aside>
+</template>
+<script>
+export default {
+  name: 'mySider',
+  data() {
+    return {
+      visible: true
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.my-header {
+  padding: 0 20px;
+  box-sizing: border-box;
+  flex-shrink: 0;
+}
+</style>
