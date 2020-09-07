4450a219a516fa635e1fdf8c712d7c802c0605a1
luo-h-x <1877430627@qq.com>
2020-09-07 23:49:57 +0800

    container

---
+++ src/components/Container.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="my-container" :class="toastClasses">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'myContainer',
+  props: {
+    direction: {
+      type: String,
+      default: 'vertical',
+      validator(value) {
+        return ['horizontal', 'vertical'].indexOf(value) >= 0
+      }
+    }
+  },
+  computed: {
+    toastClasses() {
+      return {
+        [`is-${this.direction}`]: true
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.my-container {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  box-sizing: border-box;
+  .is-horizontal {
+    flex-direction: row;
+  }
+}
+</style>
